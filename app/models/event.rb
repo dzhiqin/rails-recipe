4775f300d4ec0cad6be762f7ae23e7d75a2495e2
dzhiqin <dzhiqin@126.com>
2017-10-09 21:43:35 +0800

    set language and time zone

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,11 @@
 class Event < ApplicationRecord
-
- validates_presence_of :name
-
+  before_validation :generate_friendly_id,:on=> :create
+  validates_presence_of :name
+  def to_param
+    self.friendly_id
+  end
+  protected
+  def generate_friendly_id
+    self.friendly_id ||= SecureRandom.uuid
+  end
 end
