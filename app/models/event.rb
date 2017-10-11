790b847dd6fa9c472b50d144eefd9cbd8b1ee6b0
dzhiqin <dzhiqin@126.com>
2017-10-11 09:08:32 +0800

    add status to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
   before_validation :generate_friendly_id,:on=> :create
   validates_presence_of :name
+  belongs_to :category,:optional => true
+  STATUS=["draft","public","private"]
+  validates_inclusion_of :status,:in=> STATUS
   def to_param
     self.friendly_id
   end
