cb53fd05a0a1f8e65fb26ca208528bfb5f4cb83d
dzhiqin <dzhiqin@126.com>
2017-10-26 16:46:27 +0800

    single select conditions

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -1,13 +1,20 @@
 class Admin::EventRegistrationsController < AdminController
   before_action :find_event
   def index
-    @registrations=@event.registrations.includes(:ticket).order("id DESC")
+    @registrations=@event.registrations.includes(:ticket).order("id DESC").page(params[:page]).per(10)
+    if params[:status].present? && Registration::STATUS.include?(params[:status])
+      @registrations=@registrations.by_status(params[:status])
+    end
+    if params[:ticket_id].present?
+      @registrations=@registrations.by_ticket(params[:ticket_id])
+    end
   end
   def destroy
     @registration=@event.registrations.find_by_uuid(params[:id])
     @registration.destroy
     redirect_to admin_event_registrations_path(@event)
   end
+
   protected
   def find_event
     @event=Event.find_by_friendly_id(params[:event_id])
