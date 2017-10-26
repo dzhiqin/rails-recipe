02163ba9c8d9d45fd417a7c449214a7656ce3f2e
dzhiqin <dzhiqin@126.com>
2017-10-26 09:31:17 +0800

    add validates

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,10 +9,11 @@ class RegistrationsController < ApplicationController
     @registration.ticket=@event.tickets.find(params[:registration][:ticket_id])
     @registration.status="pending"
     @registration.user=current_user
-
+    @registration.current_step=1
     if @registration.save
       redirect_to step2_event_registration_path(@event,@registration)
     else
+      flash.now[:alert]=@registration.errors[:base].join("、")
       render "new"
     end
   end
@@ -22,9 +23,11 @@ class RegistrationsController < ApplicationController
   end
   def step1
     @registration=@event.registrations.find_by_uuid(params[:id])
+
   end
   def step1_update
     @registration=@event.registrations.find_by_uuid(params[:id])
+    @registration.current_step=1
     if @registration.update(registration_params)
       redirect_to step2_event_registration_path(@event,@registration)
     else
@@ -37,6 +40,7 @@ class RegistrationsController < ApplicationController
   end
   def step2_update
     @registration=@event.registrations.find_by_uuid(params[:id])
+    @registration.current_step=2
     if @registration.update(registration_params)
       redirect_to step3_event_registration_path(@event,@registration)
     else
@@ -48,6 +52,7 @@ class RegistrationsController < ApplicationController
   end
   def step3_update
     @registration=@event.registrations.find_by_uuid(params[:id])
+    @registration.current_step=3
     @registration.status="confirmed"
     if @registration.update(registration_params)
       flash[:notice]="报名成功"
